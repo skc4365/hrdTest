83e4f8a9acfdba77c35277a7406ef52ea824a2f6
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-10-02 17:35:41 +0900

    add shop작업중

--- ex-shopmember/src/com/HRD_ShopMember.java
+++ ex-shopmember/src/com/HRD_ShopMember.java
@@ -7,14 +7,21 @@ public class HRD_ShopMember {
 	public static void main(String[] args) {
 
 		ExClass exClass = new ExClass();
-		
+
+//		고객테이블 데이터 전체 삭제
+		exClass.deleteAll("Sale");
+		exClass.deleteAll("ShopMember");
+//		자동증가코드를 1로 초기화
+		exClass.setAuto_Increment("Sale", 1);
+		exClass.setAuto_Increment("ShopMember", 1);
+		exClass.selectAll("ShopMember");
+		exClass.selectAll("Sale");
+
 		exClass.insertShopMember("홍길동", "010-1234-5678", "서울시 강남구", "2020-01-01", 'A', "01");
 		exClass.insertShopMember("이순신", "010-2222-3333", "부산시 해운대구", "2021-03-15", 'B', "02");
 		exClass.insertShopMember("강감찬", "010-7777-8888", "대구시 달서구", "2019-05-20", 'C', "03");
 		exClass.insertShopMember("을지문덕", "010-8888-9999", "경기도 수원시", "2023-03-03", 'D', "04");
-		
 
-		
 	}
 
 }
--- ex-shopmember/src/com/ex/ExClass.java
+++ ex-shopmember/src/com/ex/ExClass.java
@@ -3,17 +3,105 @@ package com.ex;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.conn.DBConnection;
 
 public class ExClass {
-	
+
 	Connection conn = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 
+//	테이블 삭제
+	public void deleteAll(String tableName) {
+		String sql = "delete table " + tableName;
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			int rows = pstmt.executeUpdate();
+			System.out.println("----- 모든행: " + rows + "행이 삭제됨 -----");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(pstmt, conn);
+		}
+	}
+
+//	auto_increment = 1
+	public void setAuto_Increment(String tableName, int autoInit) {
+		String sql = "alter table " + tableName + " auto_increment = " + autoInit;
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			int rows = pstmt.executeUpdate();
+			System.out.println("----- " + rows + "auto_increment = 1로 셋팅됨 -----");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(pstmt, conn);
+		}
+	}
+
+//	테이블 내용 출력
+	public void selectAll(String tableName) {
+
+		try {
+			String SQL = "select * from " + tableName;
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+
+			if (tableName == "ShopMember") {
+				// rs 출력
+				System.out.println("----- 고객테이블의 전체데이터 확인 -----");
+				while (rs.next()) {
+
+					int CustNo = rs.getInt("CustNo");
+					String CustName = rs.getString("CustName");
+					String Phone = rs.getString("Phone");
+					String Address = rs.getString("Address");
+					String JoinDate = rs.getString("JoinDate");
+					String Grade = rs.getString("Grade");
+					String City = rs.getString("City");
+
+					System.out.print("회원번호:" + CustNo + "  회원성명:" + CustName + "  전화번호:" + Phone + "  주소:" + Address
+							+ "  가입일자:" + JoinDate + "  고객등급:" + Grade + "  도시코드:" + City + "\n");
+				}
+			} else if (tableName == "Sale") {
+				// rs 출력
+				System.out.println("----- 판매테이블의 전체데이터 확인 -----");
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(rs, pstmt, conn);
+		}
+	}
+
 	public void insertShopMember(String CustName, String Phone, String Address, String JoinDate, char Grade,
 			String City) {
-		// TODO Auto-generated method stub
-		
+//		String sql = "insert into ShopMember(CustName, Phone, Address, JoinDate, Grade, City) \r\n"
+//				+ "	values(?, ?, ?, ?, ?, ?)";
+//		try {
+//			conn = DBConnection.getConnection();
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, CustName);
+//			pstmt.setString(2, Phone);
+//			pstmt.setString(3, Address);
+//			pstmt.setString(4, JoinDate);
+////			pstmt.setString(5, Character.toString(Grade));
+//			pstmt.setString(5, String.valueOf(Grade));
+//			pstmt.setString(6, City);
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			// 깔끔하게 자원 해제
+//			DBConnection.close(pstmt, conn);
+//		}
+
 	}
 
 }
